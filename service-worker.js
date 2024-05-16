6b7d8bed744904d63953a5cbc7ef8e7d31a7828a
tkt4bu5 <ic4ru5.ic4ru5@gmail.com>
2024-05-16 12:24:54 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,68 +1,50 @@
-const staticCacheName = 'site-static-v2'
-const dynamicCacheName = 'site-dynamic-v1'
 const assets = [
-  "backhome.png",
-  "checkandvalidation.png",
-  "clock.png",
-  "index.html",
-  "person.png",
-  "verif.png",
-  "waves.png"
-]
+  "/tkt/",
+  "/tkt/index.html",
+  "/tkt/backhome.png",
+  "/tkt/checkandvalidation.png",
+  "/tkt/clock.png",
+  "/tkt/person.png",
+  "/tkt/verif.png",
+  "/tkt/waves.png"
+];
 
-// cache size limit function
-const limitCacheSize = (name, size) => {
-  caches.open(name).then(cache => {
-    cache.keys().then(keys => {
-      if (keys.length > size) {
-        cache.delete(keys[0]).then(limitCacheSize(name, size))
-      }
-    })
-  })
-}
-
-// install event
-self.addEventListener('install', evt => {
-  //console.log('service worker installed')
-  evt.waitUntil(
-    caches.open(staticCacheName).then((cache) => {
-      console.log('caching shell assets')
-      cache.addAll(assets)
-    })
-  )
-})
-
-// activate event
-self.addEventListener('activate', evt => {
-  //console.log('service worker activated')
-  evt.waitUntil(
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(staticCascheName).then(cache => {
+      cache.addAll(assets);
+      console.log("service worker installed -- assets cached");
+    }).catch(err => console.log("issue caching assets on install - ", err))
+  );
+});
+  
+self.addEventListener("activate", activateEvent => {
+  activateEvent.waitUntil(
     caches.keys().then(keys => {
-      //console.log(keys)
+      console.log("service worker activated");
       return Promise.all(keys
-        .filter(key => key !== staticCacheName && key !== dynamicCacheName)
+        .filter(key => key !== staticCascheName)
         .map(key => caches.delete(key))
       )
-    })
-  )
-})
+    }).catch(err => console.log("issue getting cacheKeys on activate - ", err))
+  );
+});
 
-// fetch event
-self.addEventListener('fetch', evt => {
-  //console.log('fetch event', evt)
-  evt.respondWith(
-    caches.match(evt.request).then(cacheRes => {
-      return cacheRes || fetch(evt.request).then(fetchRes => {
-        return caches.open(dynamicCacheName).then(cache => {
-          cache.put(evt.request.url, fetchRes.clone())
-          // check cached items size
-          limitCacheSize(dynamicCacheName, 15)
-          return fetchRes
+self.addEventListener("fetch", fetchEvent => {
+  fetchEvent.respondWith(
+    caches.open(staticCascheName).then(cache => {
+      console.log("fetching from service worker");
+      return cache.match(fetchEvent.request).then(res => res || fetch(fetchEvent.request)
+        .then(networkRes => {
+          console.log("puting network response to cache");
+          cache.put(fetchEvent.request, networkRes.clone());
+          return networkRes;
         })
+      ).catch(() => {
+        if(fetchEvent.request.url.indexOf(".html") > -1 || fetchEvent.request.url === "https://github.com/tkt4bu5"){
+          return caches.match("/tkt/404.html");
+        }
       })
-    }).catch(() => {
-      if (evt.request.url.indexOf('.html') > -1) {
-        return caches.match('/404.html')
-      }
-    })
-  )
-})
+    }).catch(err => console.log("issue opening cache on fetch", err))
+  );
+});
